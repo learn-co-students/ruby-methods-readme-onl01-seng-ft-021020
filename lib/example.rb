f791f2a2e7c66788ecd23ef364373cd3b4355a2d
Victor Ibasco <victoribasco18@gmail.com>
2020-02-10 17:04:27 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,8 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def phrase
+  puts "Hello World"
+end
+
+phrase
+
